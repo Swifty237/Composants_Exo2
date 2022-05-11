6df6f39dd05b249279cb6056eeb6d481ca45e5ee
yannick <yannickkamdemkouam@yahoo.fr>
2022-05-11 14:07:23 +0200

    App End

--- App.js
+++ App.js
@@ -6,31 +6,11 @@ export default function App() {
 
       <View style={styles.box}>
         <View>
-          <Image style={styles.picture} source={require('./assets/opportunity.jpg')}/>
-        </View>
-          <View>
-            <Text style={styles.title}>Title: Michael Faraday</Text>
-            <Text style={styles.description}>Description: Michael Faraday (Newington, 22 septembre 1791 - Hampton Court, 25 août 1867) est un physicien et un chimiste britannique, connu pour ses travaux fondamentaux dans le domaine de l'électromagnétisme, l'électrochimie, le diamagnétisme, et l'électrolyse.</Text>
-          </View>
-      </View>
-
-      <View style={styles.box}>
-        <View>
           <Image style={styles.picture} source={require('./assets/rover.jpg')}/>
         </View>
-        <View>
-          <Text style={styles.title}>Title: Nikola TESLA</Text>
-          <Text style={styles.description}>Description: Nikola Tesla (en serbe cyrillique : Никола Тесла), né dans la nuit du 9 au 10 juillet 1856 à Smiljan dans l'Empire d'Autriche (actuelle Croatie) et mort le 7 janvier 1943 à New York, est un inventeur et ingénieur américain d'origine serbe. Il est notoirement connu pour son rôle prépondérant dans le développement et l'adoption du courant alternatif pour le transport et la distribution de l'électricité.</Text>
-        </View>
-      </View>
-
-      <View style={styles.box}>
-        <View>
-          <Image style={styles.picture} source={require('./assets/spirit.jpg')}/>
-        </View>
-        <View>
-          <Text style={styles.title}>Title: Benjamin Franklin</Text>
-          <Text style={styles.description}>Description: Benjamin Franklin /ˈbɛnd͡ʒəmɪn ˈfɹæŋklɪn/1, né le 17 janvier 1706 à Boston et mort le 17 avril 1790 à Philadelphie, est un imprimeur, éditeur, écrivain, naturaliste, inventeur, abolitionniste et homme politique américain.</Text>
+        <View style={styles.text}>
+          <Text style={styles.title}>Title: Michael FARADAY</Text>
+          <Text style={styles.description}>Description: Michael Faraday (Newington, 22 septembre 1791 - Hampton Court, 25 août 1867) est un physicien et un chimiste britannique, connu pour ses travaux fondamentaux dans le domaine de l'électromagnétisme, l'électrochimie, le diamagnétisme, et l'électrolyse. </Text>
         </View>
       </View>
 
@@ -44,14 +24,13 @@ const styles = StyleSheet.create({
   },
   
   box: {
-    height: 250,
+    height: 300,
     flexDirection: "row",
-    marginTop: 10
+    marginTop: 10,  
   },
 
   picture: {
-    height: 200,
-    width: 200
+    width: 250
   },
 
   title: {
@@ -60,5 +39,9 @@ const styles = StyleSheet.create({
 
   description: {
     margin: 10
+  },
+
+  text: {
+    width: 100
   }
 });
